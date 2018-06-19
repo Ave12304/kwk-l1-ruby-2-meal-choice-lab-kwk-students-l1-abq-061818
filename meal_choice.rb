c862080d3f0cb8a67f415ca51e5a0a8d1dcc60ab
Ave12304 <averyselover@hotmail.com>
2018-06-19 16:45:11 +0000

    automatically backed up by learn

--- meal_choice.rb
+++ meal_choice.rb
@@ -1,14 +1,14 @@
 
 
-def breakfast(food1=pancakes)
+def breakfast(food1="pancakes")
   puts'breakfast is the right time for #{food1}'
 end
 
-def lunch(food2=tacos)
+def lunch(food2="tacos")
   puts"#{food2} is my favorite thing for lunch"
 end
 
-def dinner(food3=steak)
+def dinner(food3="steak")
   puts  "#{food3} is hard to make for dinner"
 end
 breakfast
